80bec546171f9826ddecd31ea01adb9c178933f8
Matthew Edmondson <matthew.edmondson@gmail.com>
2025-03-21 13:54:50 +1100

    Update update-readme.ps1

--- .github/workflows/update-readme.ps1
+++ .github/workflows/update-readme.ps1
@@ -22,17 +22,24 @@ do {
 
     foreach ($repo in $repos) {
         Write-Host "Processing $($repo.name)"
-        $githubVersionCheck = gh api "/repos/$org/$($repo.name)/releases/latest" -q '.tag_name' 2>$null
-        $nugetVersionCheck = Find-Package -Name "$nuget.$($repo.name)" -AllowPrereleaseVersions -AllVersions 2>$null
-        $workflowCheck = gh api "/repos/$org/$($repo.name)/actions/workflows/dotnet.yml/runs" -q '.workflow_runs[0].status' 2>$null
-        $readme += "|[$($repo.name)](https://github.com/$org/$($repo.name))"
-        if ($null -ne $nugetVersionCheck) { $readme += "|![NuGet Version](https://img.shields.io/nuget/v/$nuget.$($repo.name)?label=&logo=nuget)" }
-        elseif ($githubVersionCheck) { $readme += "|![GitHub Version](https://img.shields.io/github/v/release/$org/$($repo.name)?label=&logo=github)" }
-        else { $readme += "|N/A" }
-        $readme += "|![GitHub commit activity](https://img.shields.io/github/commit-activity/m/$org/$($repo.name)?label=&logo=github)"
-        if ($null -ne $workflowCheck) { $readme += "|![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/$org/$($repo.name)/dotnet.yml?label=&logo=github)" }
-        else { $readme += "|N/A" }
-        $readme += "|`n"
+        try
+        {
+            $readmeLine = ""
+            $githubVersionCheck = gh api "/repos/$org/$($repo.name)/releases/latest" -q '.tag_name' 2>$null
+            $nugetVersionCheck = Find-Package -Name "$nuget.$($repo.name)" -AllowPrereleaseVersions -AllVersions 2>$null
+            $workflowCheck = gh api "/repos/$org/$($repo.name)/actions/workflows/dotnet.yml/runs" -q '.workflow_runs[0].status' 2>$null
+            $readmeLine += "|[$($repo.name)](https://github.com/$org/$($repo.name))"
+            if ($null -ne $nugetVersionCheck) { $readmeLine += "|![NuGet Version](https://img.shields.io/nuget/v/$nuget.$($repo.name)?label=&logo=nuget)" }
+            elseif ($githubVersionCheck) { $readmeLine += "|![GitHub Version](https://img.shields.io/github/v/release/$org/$($repo.name)?label=&logo=github)" }
+            else { $readmeLine += "|N/A" }
+            $readmeLine += "|![GitHub commit activity](https://img.shields.io/github/commit-activity/m/$org/$($repo.name)?label=&logo=github)"
+            if ($null -ne $workflowCheck) { $readmeLine += "|![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/$org/$($repo.name)/dotnet.yml?label=&logo=github)" }
+            else { $readmeLine += "|N/A" }
+            $readmeLine += "|`n"
+            $readme += readmeLine
+        } catch {
+            Write-Output "$_"
+        }
     }
     $page++
 } while ($repos.Count -eq $per_page)
